d85d6c99e6bbf32207bbe2c028628433d9cc65ef
Trevor254 <trevoromondi607@gmail.com>
2020-01-31 15:10:36 +0300

    add get request to route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,18 @@
+var ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app,db){
+    //get data from server
+    app.get('/notes/:id',((req,res)=>{
+        const id = req.params.id
+        const details = { '_id' : new ObjectID(id) };
+            db.collection('notes').findOne(details,(err,item)=>{
+                if(err){
+                    res.send({'error':'An error has occured'})
+                } else {
+                    res.send(item)
+                }
+            })
+    }))
     app.post('/notes',(req,res)=>{
         //create note here
         const note = { text:req.body.body, title:req.body.title }; //req.body.body gets the body variable from the request
@@ -10,4 +24,5 @@ module.exports = function(app,db){
            }
        })
     })
-}
+};
+
